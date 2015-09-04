d827332ca01e9684d42d6abac1d91d2a8455c4c4
Oleksandr Natalenko <o.natalenko@lanet.ua>
2015-09-04 12:36:53 +0300

    acl: update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -1,17 +1,28 @@
+CREATE TABLE `lists` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `items` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`list` bigint(20) unsigned NOT NULL,
+	`value` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `list_index` (`list`),
+	CONSTRAINT `items_ibfk_1` FOREIGN KEY (`list`) REFERENCES `lists` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `rules` (
 	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	`protocol` tinyint(3) unsigned NOT NULL,
 	`ip` varchar(46) NOT NULL,
 	`mask` varchar(46) NOT NULL,
-	`list` varchar(255) NOT NULL,
+	`matcher` varchar(255) NOT NULL,
+	`list` bigint(20) unsigned NOT NULL,
 	`action` varchar(255) NOT NULL,
 	`action_parameters` varchar(255) DEFAULT NULL,
-	PRIMARY KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
-CREATE TABLE `lists` (
-	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`regex` varchar(255) NOT NULL,
-	PRIMARY KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+	PRIMARY KEY (`id`),
+	KEY `list_index` (`list`),
+	CONSTRAINT `rules_ibfk_1` FOREIGN KEY (`list`) REFERENCES `lists` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
