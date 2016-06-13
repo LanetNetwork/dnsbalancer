8c6851e39fa13cafb6be1707a02540dea0aabfd3
Oleksandr Natalenko <o.natalenko@lanet.ua>
2016-06-13 10:04:30 +0300

    contrib/xxhash: indent -linux

--- contrib/xxhash/xxhash.h
+++ contrib/xxhash/xxhash.h
@@ -71,13 +71,11 @@ XXH32        6.8 GB/s            6.0 GB/s
 extern "C" {
 #endif
 
-
 /* ****************************
 *  Definitions
 ******************************/
-#include <stddef.h>   /* size_t */
-typedef enum { XXH_OK=0, XXH_ERROR } XXH_errorcode;
-
+#include <stddef.h>		/* size_t */
+	typedef enum { XXH_OK = 0, XXH_ERROR } XXH_errorcode;
 
 /* ****************************
 *  API modifier
@@ -92,17 +90,17 @@ typedef enum { XXH_OK=0, XXH_ERROR } XXH_errorcode;
 *  also : don't compile and link xxhash.c separately
 */
 #ifdef XXH_PRIVATE_API
-#  if defined(__GNUC__)
-#    define XXH_PUBLIC_API static __attribute__((unused))
-#  elif defined (__cplusplus) || (defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) /* C99 */)
-#    define XXH_PUBLIC_API static inline
-#  elif defined(_MSC_VER)
-#    define XXH_PUBLIC_API static __inline
-#  else
-#    define XXH_PUBLIC_API static   /* this version may generate warnings for unused static functions; disable the relevant warning */
-#  endif
+#if defined(__GNUC__)
+#define XXH_PUBLIC_API static __attribute__((unused))
+#elif defined (__cplusplus) || (defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) /* C99 */)
+#define XXH_PUBLIC_API static inline
+#elif defined(_MSC_VER)
+#define XXH_PUBLIC_API static __inline
 #else
-#  define XXH_PUBLIC_API   /* do nothing */
+#define XXH_PUBLIC_API static	/* this version may generate warnings for unused static functions; disable the relevant warning */
+#endif
+#else
+#define XXH_PUBLIC_API		/* do nothing */
 #endif
 
 /*!XXH_NAMESPACE, aka Namespace Emulation :
@@ -117,23 +115,22 @@ Note that no change is required within the calling program as long as it also in
 regular symbol name will be automatically translated by this header.
 */
 #ifdef XXH_NAMESPACE
-#  define XXH_CAT(A,B) A##B
-#  define XXH_NAME2(A,B) XXH_CAT(A,B)
-#  define XXH32 XXH_NAME2(XXH_NAMESPACE, XXH32)
-#  define XXH64 XXH_NAME2(XXH_NAMESPACE, XXH64)
-#  define XXH_versionNumber XXH_NAME2(XXH_NAMESPACE, XXH_versionNumber)
-#  define XXH32_createState XXH_NAME2(XXH_NAMESPACE, XXH32_createState)
-#  define XXH64_createState XXH_NAME2(XXH_NAMESPACE, XXH64_createState)
-#  define XXH32_freeState XXH_NAME2(XXH_NAMESPACE, XXH32_freeState)
-#  define XXH64_freeState XXH_NAME2(XXH_NAMESPACE, XXH64_freeState)
-#  define XXH32_reset XXH_NAME2(XXH_NAMESPACE, XXH32_reset)
-#  define XXH64_reset XXH_NAME2(XXH_NAMESPACE, XXH64_reset)
-#  define XXH32_update XXH_NAME2(XXH_NAMESPACE, XXH32_update)
-#  define XXH64_update XXH_NAME2(XXH_NAMESPACE, XXH64_update)
-#  define XXH32_digest XXH_NAME2(XXH_NAMESPACE, XXH32_digest)
-#  define XXH64_digest XXH_NAME2(XXH_NAMESPACE, XXH64_digest)
+#define XXH_CAT(A,B) A##B
+#define XXH_NAME2(A,B) XXH_CAT(A,B)
+#define XXH32 XXH_NAME2(XXH_NAMESPACE, XXH32)
+#define XXH64 XXH_NAME2(XXH_NAMESPACE, XXH64)
+#define XXH_versionNumber XXH_NAME2(XXH_NAMESPACE, XXH_versionNumber)
+#define XXH32_createState XXH_NAME2(XXH_NAMESPACE, XXH32_createState)
+#define XXH64_createState XXH_NAME2(XXH_NAMESPACE, XXH64_createState)
+#define XXH32_freeState XXH_NAME2(XXH_NAMESPACE, XXH32_freeState)
+#define XXH64_freeState XXH_NAME2(XXH_NAMESPACE, XXH64_freeState)
+#define XXH32_reset XXH_NAME2(XXH_NAMESPACE, XXH32_reset)
+#define XXH64_reset XXH_NAME2(XXH_NAMESPACE, XXH64_reset)
+#define XXH32_update XXH_NAME2(XXH_NAMESPACE, XXH32_update)
+#define XXH64_update XXH_NAME2(XXH_NAMESPACE, XXH64_update)
+#define XXH32_digest XXH_NAME2(XXH_NAMESPACE, XXH32_digest)
+#define XXH64_digest XXH_NAME2(XXH_NAMESPACE, XXH64_digest)
 #endif
-
 
 /* *************************************
 *  Version
@@ -142,17 +139,18 @@ regular symbol name will be automatically translated by this header.
 #define XXH_VERSION_MINOR    5
 #define XXH_VERSION_RELEASE  0
 #define XXH_VERSION_NUMBER  (XXH_VERSION_MAJOR *100*100 + XXH_VERSION_MINOR *100 + XXH_VERSION_RELEASE)
-XXH_PUBLIC_API unsigned XXH_versionNumber (void);
-
+	XXH_PUBLIC_API unsigned XXH_versionNumber(void);
 
 /* ****************************
 *  Simple Hash Functions
 ******************************/
-typedef unsigned int       XXH32_hash_t;
-typedef unsigned long long XXH64_hash_t;
+	typedef unsigned int XXH32_hash_t;
+	typedef unsigned long long XXH64_hash_t;
 
-XXH_PUBLIC_API XXH32_hash_t XXH32 (const void* input, size_t length, unsigned int seed);
-XXH_PUBLIC_API XXH64_hash_t XXH64 (const void* input, size_t length, unsigned long long seed);
+	XXH_PUBLIC_API XXH32_hash_t XXH32(const void *input, size_t length,
+					  unsigned int seed);
+	XXH_PUBLIC_API XXH64_hash_t XXH64(const void *input, size_t length,
+					  unsigned long long seed);
 
 /*!
 XXH32() :
@@ -166,13 +164,11 @@ XXH64() :
     This function runs faster on 64-bits systems, but slower on 32-bits systems (see benchmark).
 */
 
-
 /* ****************************
 *  Streaming Hash Functions
 ******************************/
-typedef struct XXH32_state_s XXH32_state_t;   /* incomplete type */
-typedef struct XXH64_state_s XXH64_state_t;   /* incomplete type */
-
+	typedef struct XXH32_state_s XXH32_state_t;	/* incomplete type */
+	typedef struct XXH64_state_s XXH64_state_t;	/* incomplete type */
 
 /*! Static allocation
     For static linking only, do not use in the context of DLL !
@@ -180,32 +176,42 @@ typedef struct XXH64_state_s XXH64_state_t;   /* incomplete type */
             is static-allocation equivalent of :
         XXHnn_state_t* name = XXHnn_createState();
 */
-typedef struct { long long ll[ 6]; } XXH32_stateBody_t;
-typedef struct { long long ll[11]; } XXH64_stateBody_t;
+	typedef struct {
+		long long ll[6];
+	} XXH32_stateBody_t;
+	typedef struct {
+		long long ll[11];
+	} XXH64_stateBody_t;
 
-#define XXH32_CREATESTATE_STATIC(name) XXH32_stateBody_t name##xxhbody; void* name##xxhvoid = &(name##xxhbody); XXH32_state_t* name = (XXH32_state_t*)(name##xxhvoid)   /* no final ; */
-#define XXH64_CREATESTATE_STATIC(name) XXH64_stateBody_t name##xxhbody; void* name##xxhvoid = &(name##xxhbody); XXH64_state_t* name = (XXH64_state_t*)(name##xxhvoid)   /* no final ; */
-
+#define XXH32_CREATESTATE_STATIC(name) XXH32_stateBody_t name##xxhbody; void* name##xxhvoid = &(name##xxhbody); XXH32_state_t* name = (XXH32_state_t*)(name##xxhvoid)	/* no final ; */
+#define XXH64_CREATESTATE_STATIC(name) XXH64_stateBody_t name##xxhbody; void* name##xxhvoid = &(name##xxhbody); XXH64_state_t* name = (XXH64_state_t*)(name##xxhvoid)	/* no final ; */
 
 /*!Dynamic allocation
    To be preferred in the context of DLL */
 
-XXH_PUBLIC_API XXH32_state_t* XXH32_createState(void);
-XXH_PUBLIC_API XXH_errorcode  XXH32_freeState(XXH32_state_t* statePtr);
+	XXH_PUBLIC_API XXH32_state_t *XXH32_createState(void);
+	XXH_PUBLIC_API XXH_errorcode XXH32_freeState(XXH32_state_t * statePtr);
 
-XXH_PUBLIC_API XXH64_state_t* XXH64_createState(void);
-XXH_PUBLIC_API XXH_errorcode  XXH64_freeState(XXH64_state_t* statePtr);
-
+	XXH_PUBLIC_API XXH64_state_t *XXH64_createState(void);
+	XXH_PUBLIC_API XXH_errorcode XXH64_freeState(XXH64_state_t * statePtr);
 
 /* hash streaming */
 
-XXH_PUBLIC_API XXH_errorcode XXH32_reset  (XXH32_state_t* statePtr, unsigned int seed);
-XXH_PUBLIC_API XXH_errorcode XXH32_update (XXH32_state_t* statePtr, const void* input, size_t length);
-XXH_PUBLIC_API XXH32_hash_t  XXH32_digest (const XXH32_state_t* statePtr);
+	XXH_PUBLIC_API XXH_errorcode XXH32_reset(XXH32_state_t * statePtr,
+						 unsigned int seed);
+	XXH_PUBLIC_API XXH_errorcode XXH32_update(XXH32_state_t * statePtr,
+						  const void *input,
+						  size_t length);
+	XXH_PUBLIC_API XXH32_hash_t XXH32_digest(const XXH32_state_t *
+						 statePtr);
 
-XXH_PUBLIC_API XXH_errorcode XXH64_reset  (XXH64_state_t* statePtr, unsigned long long seed);
-XXH_PUBLIC_API XXH_errorcode XXH64_update (XXH64_state_t* statePtr, const void* input, size_t length);
-XXH_PUBLIC_API XXH64_hash_t  XXH64_digest (const XXH64_state_t* statePtr);
+	XXH_PUBLIC_API XXH_errorcode XXH64_reset(XXH64_state_t * statePtr,
+						 unsigned long long seed);
+	XXH_PUBLIC_API XXH_errorcode XXH64_update(XXH64_state_t * statePtr,
+						  const void *input,
+						  size_t length);
+	XXH_PUBLIC_API XXH64_hash_t XXH64_digest(const XXH64_state_t *
+						 statePtr);
 
 /*!
 These functions generate the xxHash of an input provided in multiple segments,
@@ -228,18 +234,27 @@ and later on generate some new hashes, by calling again XXHnn_digest().
 When done, free XXH state space if it was allocated dynamically.
 */
 
-
 /* **************************
 *  Canonical representation
 ****************************/
-typedef struct { unsigned char digest[4]; } XXH32_canonical_t;
-typedef struct { unsigned char digest[8]; } XXH64_canonical_t;
+	typedef struct {
+		unsigned char digest[4];
+	} XXH32_canonical_t;
+	typedef struct {
+		unsigned char digest[8];
+	} XXH64_canonical_t;
 
-XXH_PUBLIC_API void XXH32_canonicalFromHash(XXH32_canonical_t* dst, XXH32_hash_t hash);
-XXH_PUBLIC_API void XXH64_canonicalFromHash(XXH64_canonical_t* dst, XXH64_hash_t hash);
+	XXH_PUBLIC_API void XXH32_canonicalFromHash(XXH32_canonical_t * dst,
+						    XXH32_hash_t hash);
+	XXH_PUBLIC_API void XXH64_canonicalFromHash(XXH64_canonical_t * dst,
+						    XXH64_hash_t hash);
 
-XXH_PUBLIC_API XXH32_hash_t XXH32_hashFromCanonical(const XXH32_canonical_t* src);
-XXH_PUBLIC_API XXH64_hash_t XXH64_hashFromCanonical(const XXH64_canonical_t* src);
+	XXH_PUBLIC_API XXH32_hash_t XXH32_hashFromCanonical(const
+							    XXH32_canonical_t *
+							    src);
+	XXH_PUBLIC_API XXH64_hash_t XXH64_hashFromCanonical(const
+							    XXH64_canonical_t *
+							    src);
 
 /*! Default result type for XXH functions are primitive unsigned 32 and 64 bits.
 *   The canonical representation uses human-readable write convention, aka big-endian (large digits first).
@@ -247,9 +262,7 @@ XXH_PUBLIC_API XXH64_hash_t XXH64_hashFromCanonical(const XXH64_canonical_t* src
 *   This way, hash values can be written into a file / memory, and remain comparable on different systems and programs.
 */
 
-
 #if defined (__cplusplus)
 }
 #endif
-
-#endif /* XXHASH_H_5627135585666179 */
+#endif				/* XXHASH_H_5627135585666179 */
