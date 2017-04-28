LanetNetwork/dnsbalancer

commit bb008de63c72120de0b2fe2b1860af9089674682

core: refine notifications
1) Do not send exit notifications manually from main.c
to each worker, employ global notification socket with
proper eventfd usage.

This removes direct intrusion in worker context data.

2) Make GC timers local for each worker.

This resolves improper GC timer broadcast invokation,
when not all threads could be woken up.

context.c
@@ -409,11 +409,11 @@ struct ds_ctx* ds_ctx_load(const char* _config_file)
 
 	ret->req_ttl = ds_cfg_get_u64(cfg, DS_CFG_SECTION_GENERAL,
 								  DS_CFG_KEY_REQ_TTL, DS_CFG_DEFAULT_REQ_TTL) * 1000000ULL;
-	ret->gc_fd = ds_timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
-	ds_timerfd_settime(ret->gc_fd,
-					   ds_cfg_get_u64(cfg, DS_CFG_SECTION_GENERAL,
-									  DS_CFG_KEY_GC_INTVL,
-									  DS_CFG_DEFAULT_GC_INTVL) * 1000000ULL);
+	ret->gc_intvl = ds_cfg_get_u64(cfg, DS_CFG_SECTION_GENERAL,
+								   DS_CFG_KEY_GC_INTVL,
+								   DS_CFG_DEFAULT_GC_INTVL) * 1000000ULL;
+
+	ret->exit_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
 
 	ret->nwrks = pfcq_hint_cpus(ds_cfg_get_int(cfg, DS_CFG_SECTION_GENERAL, DS_CFG_KEY_WRKS, DS_CFG_DEFAULT_WRKS));
 	ret->wrks = pfcq_alloc(ret->nwrks * sizeof(struct ds_wrk_ctx*));
@@ -447,7 +447,7 @@ void ds_ctx_unload(struct ds_ctx* _ctx)
 	}
 	pfcq_free(_ctx->wrks);
 
-	ds_close(_ctx->gc_fd);
+	ds_close(_ctx->exit_fd);
 	ds_close(_ctx->wdt_fd);
 
 	pfcq_counter_reset(&_ctx->in_flight);

evloop.c
@@ -65,14 +65,14 @@ int ds_wrk_loop_handler(struct epoll_event _event, struct ds_wrk_ctx* _data)
 	}
 
 	// perform exit
-	if (_event.data.fd == _data->notify_exit_fd)
+	if (_event.data.fd == _data->ctx->exit_fd)
 	{
 		ret = ds_wrk_exit_handler(_event.data.fd, _data);
 		goto out;
 	}
 
 	// perform tracking tree GC
-	if (_event.data.fd == _data->ctx->gc_fd)
+	if (_event.data.fd == _data->ev_gc_fd)
 	{
 		ret = ds_wrk_gc_handler(_event.data.fd, _data);
 		goto out;

main.c
@@ -140,8 +140,7 @@ int main(int _argc, char** _argv)
 
 		if (should_exit)
 		{
-			for (size_t i = 0; i < ctx->nwrks; i++)
-				ds_produce_u64(ctx->wrks[i]->notify_exit_fd);
+			ds_produce_u64_val(ctx->exit_fd, ctx->nwrks);
 
 			ds_ctx_unload(ctx);
 
@@ -155,8 +154,7 @@ int main(int _argc, char** _argv)
 			ctx->ctx_next = ctx_next;
 			ctx->redirect = true;
 
-			for (size_t i = 0; i < ctx->nwrks; i++)
-				ds_produce_u64(ctx->wrks[i]->notify_exit_fd);
+			ds_produce_u64_val(ctx->exit_fd, ctx->nwrks);
 
 			ds_ctx_unload(ctx);
 

types.h
@@ -218,12 +218,11 @@ struct ds_wrk_ctx
 	int poll_timeo;
 	int wrk_fd;
 
-	int notify_exit_fd;
-
 	int ev_prep_fd;
 	int ev_fwd_fd;
 	int ev_rep_fd;
 	int ev_wdt_rep_fd;
+	int ev_gc_fd;
 	struct ds_wrk_tsk_list prep_queue;
 	struct ds_wrk_tsk_list fwd_queue;
 	struct ds_wrk_tsk_list rep_queue;
@@ -248,10 +247,11 @@ struct ds_ctx
 	struct ds_acl_req* acl_reqs;
 	size_t n_acl_acts;
 	struct ds_acl_act* acl_acts;
-	int gc_fd;
 	int wdt_fd;
 	int tk_fd;
+	int exit_fd;
 	uint64_t req_ttl;
+	uint64_t gc_intvl;
 	struct ds_wrk_ctx** wrks;
 	size_t nwrks;
 	int poll_timeo;

utils.c
@@ -153,12 +153,18 @@ void ds_epoll_del_fd(int _evfd, int _fd)
 		panic("epoll_ctl");
 }
 
-void ds_produce_u64(int _fd)
+void ds_produce_u64_val(int _fd, uint64_t _value)
 {
-	uint64_t value = 1;
 	__attribute__((unused)) int res = 0;
 
-	res = ds_write(_fd, &value, sizeof(uint64_t));
+	res = ds_write(_fd, &_value, sizeof(uint64_t));
+
+	return;
+}
+
+void ds_produce_u64(int _fd)
+{
+	ds_produce_u64_val(_fd, 1);
 
 	return;
 }

utils.h
@@ -36,6 +36,7 @@ int ds_fwd_sk_cmp(const void* _p1, const void* _p2, void* _param) __attribute__(
 int ds_tsk_cmp(const void* _p1, const void* _p2, void* _param) __attribute__((warn_unused_result));
 void ds_epoll_add_fd(int _evfd, int _fd, uint32_t _events);
 void ds_epoll_del_fd(int _evfd, int _fd);
+void ds_produce_u64_val(int _fd, uint64_t _value);
 void ds_produce_u64(int _fd);
 void ds_consume_u64(int _fd);
 int ds_try_consume_u64(int _fd) __attribute__((warn_unused_result));

worker.c
@@ -19,6 +19,7 @@
  */
 
 #include <sys/eventfd.h>
+#include <sys/timerfd.h>
 
 #include "evloop.h"
 #include "pfcq.h"
@@ -91,6 +92,8 @@ void* ds_wrk(void* _data)
 	data->ev_fwd_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
 	data->ev_rep_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
 	data->ev_wdt_rep_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
+	data->ev_gc_fd = ds_timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
+	ds_timerfd_settime(data->ev_gc_fd, data->ctx->gc_intvl);
 	TAILQ_INIT(&data->prep_queue);
 	TAILQ_INIT(&data->fwd_queue);
 	TAILQ_INIT(&data->rep_queue);
@@ -101,11 +104,9 @@ void* ds_wrk(void* _data)
 	ds_epoll_add_fd(data->wrk_fd, data->ev_fwd_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ev_rep_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ev_wdt_rep_fd, EPOLLIN);
+	ds_epoll_add_fd(data->wrk_fd, data->ev_gc_fd, EPOLLIN);
 
-	data->notify_exit_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
-	ds_epoll_add_fd(data->wrk_fd, data->notify_exit_fd, EPOLLIN);
-
-	ds_epoll_add_fd(data->wrk_fd, data->ctx->gc_fd, EPOLLIN);
+	ds_epoll_add_fd(data->wrk_fd, data->ctx->exit_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ctx->wdt_fd, EPOLLIN | EPOLLEXCLUSIVE);
 	ds_epoll_add_fd(data->wrk_fd, data->ctx->tk_fd, EPOLLIN | EPOLLEXCLUSIVE);
 
@@ -131,24 +132,24 @@ void* ds_wrk(void* _data)
 	} while (likely((cur_fwd_wdt_sk = rb_t_next(&iter)) != NULL));
 	rb_destroy(data->fwd_wdt_sk_set, ds_rb_item_free);
 
-	ds_epoll_del_fd(data->wrk_fd, data->ctx->gc_fd);
+	ds_epoll_del_fd(data->wrk_fd, data->ctx->exit_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ctx->tk_fd);
-	ds_epoll_del_fd(data->wrk_fd, data->notify_exit_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_prep_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_fwd_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_rep_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_wdt_rep_fd);
+	ds_epoll_del_fd(data->wrk_fd, data->ev_gc_fd);
 
 	ds_close(data->ev_prep_fd);
 	ds_close(data->ev_fwd_fd);
 	ds_close(data->ev_rep_fd);
 	ds_close(data->ev_wdt_rep_fd);
+	ds_close(data->ev_gc_fd);
 
 	pfcq_spin_done(&data->rep_queue_lock);
 	pfcq_spin_done(&data->wdt_rep_queue_lock);
 
 	ds_close(data->wrk_fd);
-	ds_close(data->notify_exit_fd);
 
 	return NULL;
 }

commit 18d157859af9bb32ec4957fa8f3da9ed2b5c3fe8

core: make exit notification local again
Broadcast notifying does not really work
as any worker could be woken up, resulting in
double socket closing and other inappropriate things.

This commit introduces local exit notifications back
with small code refactoring.

Fixes: bb008de63c72120de0b2fe2b1860af9089674682
Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

context.c
@@ -413,8 +413,6 @@ struct ds_ctx* ds_ctx_load(const char* _config_file)
 								   DS_CFG_KEY_GC_INTVL,
 								   DS_CFG_DEFAULT_GC_INTVL) * 1000000ULL;
 
-	ret->exit_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
-
 	ret->nwrks = pfcq_hint_cpus(ds_cfg_get_int(cfg, DS_CFG_SECTION_GENERAL, DS_CFG_KEY_WRKS, DS_CFG_DEFAULT_WRKS));
 	ret->wrks = pfcq_alloc(ret->nwrks * sizeof(struct ds_wrk_ctx*));
 	for (size_t i = 0; i < ret->nwrks; i++)
@@ -447,7 +445,6 @@ void ds_ctx_unload(struct ds_ctx* _ctx)
 	}
 	pfcq_free(_ctx->wrks);
 
-	ds_close(_ctx->exit_fd);
 	ds_close(_ctx->wdt_fd);
 
 	pfcq_counter_reset(&_ctx->in_flight);

evloop.c
@@ -65,7 +65,7 @@ int ds_wrk_loop_handler(struct epoll_event _event, struct ds_wrk_ctx* _data)
 	}
 
 	// perform exit
-	if (_event.data.fd == _data->ctx->exit_fd)
+	if (_event.data.fd == _data->ev_exit_fd)
 	{
 		ret = ds_wrk_exit_handler(_event.data.fd, _data);
 		goto out;

main.c
@@ -140,7 +140,8 @@ int main(int _argc, char** _argv)
 
 		if (should_exit)
 		{
-			ds_produce_u64_val(ctx->exit_fd, ctx->nwrks);
+			for (size_t i = 0; i < ctx->nwrks; i++)
+				ds_produce_u64(ctx->wrks[i]->ev_exit_fd);
 
 			ds_ctx_unload(ctx);
 
@@ -154,7 +155,8 @@ int main(int _argc, char** _argv)
 			ctx->ctx_next = ctx_next;
 			ctx->redirect = true;
 
-			ds_produce_u64_val(ctx->exit_fd, ctx->nwrks);
+			for (size_t i = 0; i < ctx->nwrks; i++)
+				ds_produce_u64(ctx->wrks[i]->ev_exit_fd);
 
 			ds_ctx_unload(ctx);
 

types.h
@@ -223,6 +223,7 @@ struct ds_wrk_ctx
 	int ev_rep_fd;
 	int ev_wdt_rep_fd;
 	int ev_gc_fd;
+	int ev_exit_fd;
 	struct ds_wrk_tsk_list prep_queue;
 	struct ds_wrk_tsk_list fwd_queue;
 	struct ds_wrk_tsk_list rep_queue;
@@ -249,7 +250,6 @@ struct ds_ctx
 	struct ds_acl_act* acl_acts;
 	int wdt_fd;
 	int tk_fd;
-	int exit_fd;
 	uint64_t req_ttl;
 	uint64_t gc_intvl;
 	struct ds_wrk_ctx** wrks;

utils.c
@@ -153,18 +153,12 @@ void ds_epoll_del_fd(int _evfd, int _fd)
 		panic("epoll_ctl");
 }
 
-void ds_produce_u64_val(int _fd, uint64_t _value)
-{
-	__attribute__((unused)) int res = 0;
-
-	res = ds_write(_fd, &_value, sizeof(uint64_t));
-
-	return;
-}
-
 void ds_produce_u64(int _fd)
 {
-	ds_produce_u64_val(_fd, 1);
+	uint64_t value = 1;
+	__attribute__((unused)) int res = 0;
+
+	res = ds_write(_fd, &value, sizeof(uint64_t));
 
 	return;
 }

utils.h
@@ -36,7 +36,6 @@ int ds_fwd_sk_cmp(const void* _p1, const void* _p2, void* _param) __attribute__(
 int ds_tsk_cmp(const void* _p1, const void* _p2, void* _param) __attribute__((warn_unused_result));
 void ds_epoll_add_fd(int _evfd, int _fd, uint32_t _events);
 void ds_epoll_del_fd(int _evfd, int _fd);
-void ds_produce_u64_val(int _fd, uint64_t _value);
 void ds_produce_u64(int _fd);
 void ds_consume_u64(int _fd);
 int ds_try_consume_u64(int _fd) __attribute__((warn_unused_result));

worker.c
@@ -92,6 +92,7 @@ void* ds_wrk(void* _data)
 	data->ev_fwd_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
 	data->ev_rep_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
 	data->ev_wdt_rep_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
+	data->ev_exit_fd = ds_eventfd(0, EFD_SEMAPHORE | EFD_NONBLOCK);
 	data->ev_gc_fd = ds_timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
 	ds_timerfd_settime(data->ev_gc_fd, data->ctx->gc_intvl);
 	TAILQ_INIT(&data->prep_queue);
@@ -104,9 +105,9 @@ void* ds_wrk(void* _data)
 	ds_epoll_add_fd(data->wrk_fd, data->ev_fwd_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ev_rep_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ev_wdt_rep_fd, EPOLLIN);
+	ds_epoll_add_fd(data->wrk_fd, data->ev_exit_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ev_gc_fd, EPOLLIN);
 
-	ds_epoll_add_fd(data->wrk_fd, data->ctx->exit_fd, EPOLLIN);
 	ds_epoll_add_fd(data->wrk_fd, data->ctx->wdt_fd, EPOLLIN | EPOLLEXCLUSIVE);
 	ds_epoll_add_fd(data->wrk_fd, data->ctx->tk_fd, EPOLLIN | EPOLLEXCLUSIVE);
 
@@ -132,18 +133,19 @@ void* ds_wrk(void* _data)
 	} while (likely((cur_fwd_wdt_sk = rb_t_next(&iter)) != NULL));
 	rb_destroy(data->fwd_wdt_sk_set, ds_rb_item_free);
 
-	ds_epoll_del_fd(data->wrk_fd, data->ctx->exit_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ctx->tk_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_prep_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_fwd_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_rep_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_wdt_rep_fd);
 	ds_epoll_del_fd(data->wrk_fd, data->ev_gc_fd);
+	ds_epoll_del_fd(data->wrk_fd, data->ev_exit_fd);
 
 	ds_close(data->ev_prep_fd);
 	ds_close(data->ev_fwd_fd);
 	ds_close(data->ev_rep_fd);
 	ds_close(data->ev_wdt_rep_fd);
+	ds_close(data->ev_exit_fd);
 	ds_close(data->ev_gc_fd);
 
 	pfcq_spin_done(&data->rep_queue_lock);