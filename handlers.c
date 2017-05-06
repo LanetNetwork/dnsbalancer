LanetNetwork/dnsbalancer

commit 9a57e4a346817509493eed67bce3abeef23bd669

handlers: deal with redirected WDT reply correctly
In WDT reply handler previous context data are referenced
in case task is redirected. This is not a correct behavior,
previous context must not be referenced as there are no
guarantees data are still there.

This commit saves forwarder address into task struture
and uses it to match forwarder in WDT reply handler in case
task is redirected.

Signed-off-by: Oleksandr Natalenko <oleksandr@redhat.com>

handlers.c
@@ -162,6 +162,7 @@ int ds_wrk_obt_handler(struct ds_fwd_sk* _fwd_sk, struct ds_wrk_ctx* _data)
 	tsk->orig_fe_sk = found->orig_fe_sk;
 	tsk->orig_fe_addr = found->orig_fe_addr;
 	tsk->fe_fwd = found->fe_fwd;
+	tsk->fwd_sk_addr = found->fwd_sk_addr;
 
 	pfcq_counter_reset(&found->epoch);
 	ds_tsk_free(found);
@@ -225,7 +226,7 @@ int ds_wrk_rep_handler(int _fd, struct ds_wrk_ctx* _data)
 		rb_t_init(&iter, _data->fe_sk_set);
 		cur = rb_t_first(&iter, _data->fe_sk_set);
 		do {
-			if (pfcq_net_addr_cmp(&cur->fe->addr, &tsk->orig_fe_addr))
+			if (pfcq_net_addr_port_cmp(&cur->fe->addr, &tsk->orig_fe_addr))
 			{
 				sk = cur->sk;
 				found = true;
@@ -392,6 +393,7 @@ int ds_wrk_wdt_req_handler(int _fd, struct ds_wrk_ctx* _data)
 
 		tsk->type = DS_TSK_WDT;
 		tsk->fwd_sk = cur_fwd_wdt_sk;
+		tsk->fwd_sk_addr = tsk->fwd_sk->fwd->addr;
 
 		if (unlikely(ds_tsk_buf_parse(_data, tsk, DS_PKT_REQ) == -1))
 		{
@@ -419,6 +421,7 @@ ok:
 int ds_wrk_wdt_rep_handler(int _fd, struct ds_wrk_ctx* _data)
 {
 	struct ds_wrk_tsk* tsk = NULL;
+	struct ds_fwd_sk* fwd_wdt_sk = NULL;
 
 	ds_consume_u64(_fd);
 
@@ -427,13 +430,38 @@ int ds_wrk_wdt_rep_handler(int _fd, struct ds_wrk_ctx* _data)
 	TAILQ_REMOVE(&_data->wdt_rep_queue, tsk, tailq);
 	pfcq_spin_unlock(&_data->wdt_rep_queue_lock);
 
-	pfcq_counter_reset(&tsk->fwd_sk->fwd->wdt_pending);
-	if (unlikely(!tsk->fwd_sk->fwd->alive))
+	if (unlikely(tsk->redirected))
 	{
-		tsk->fwd_sk->fwd->alive = true;
-		verbose("Forwarder %s became reachable\n", tsk->fwd_sk->fwd->name);
+		struct rb_traverser iter;
+		struct ds_fwd_sk* cur = NULL;
+		bool found = false;
+
+		rb_t_init(&iter, _data->fwd_wdt_sk_set);
+		cur = rb_t_first(&iter, _data->fwd_wdt_sk_set);
+		do {
+			if (pfcq_net_addr_cmp(&cur->fwd->addr, &tsk->fwd_sk_addr))
+			{
+				fwd_wdt_sk = cur;
+				found = true;
+				break;
+			}
+		} while (likely((cur = rb_t_next(&iter)) != NULL));
+
+		if (!found)
+			goto out;
+	} else
+	{
+		fwd_wdt_sk = tsk->fwd_sk;
 	}
 
+	pfcq_counter_reset(&fwd_wdt_sk->fwd->wdt_pending);
+	if (unlikely(!fwd_wdt_sk->fwd->alive))
+	{
+		fwd_wdt_sk->fwd->alive = true;
+		verbose("Forwarder %s became reachable\n", fwd_wdt_sk->fwd->name);
+	}
+
+out:
 	ds_tsk_free(tsk);
 
 	pfcq_counter_dec(&_data->ctx->in_flight);

pfcq.c
@@ -431,6 +431,26 @@ bool pfcq_net_addr_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2)
 		switch (_na1->family)
 		{
 			case AF_INET:
+				return _na1->addr.ip4.sin_addr.s_addr == _na2->addr.ip4.sin_addr.s_addr;
+				break;
+			case AF_INET6:
+				return _na1->addr.ip6.sin6_addr.s6_addr == _na2->addr.ip6.sin6_addr.s6_addr;
+				break;
+			default:
+				panic("Unknown address family");
+				break;
+		}
+	} else
+		return false;
+}
+
+bool pfcq_net_addr_port_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2)
+{
+	if (_na1->family == _na2->family)
+	{
+		switch (_na1->family)
+		{
+			case AF_INET:
 				return ((_na1->addr.ip4.sin_addr.s_addr == _na2->addr.ip4.sin_addr.s_addr) &&
 						(_na1->addr.ip4.sin_port == _na2->addr.ip4.sin_port));
 				break;

pfcq.h
@@ -129,6 +129,7 @@ void pfcq_spin_done(pthread_spinlock_t* _lock);
 uint64_t pfcq_fast_hash(const uint8_t* _data, size_t _data_size, uint64_t _seed) __attribute__((warn_unused_result));
 
 bool pfcq_net_addr_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2) __attribute__((warn_unused_result));
+bool pfcq_net_addr_port_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2) __attribute__((warn_unused_result));
 
 void pfcq_counter_init(struct pfcq_counter* _counter);
 void pfcq_counter_reset(struct pfcq_counter* _counter);

dns.c
@@ -126,6 +126,7 @@ void ds_tsk_get_fwd(struct ds_wrk_tsk* _tsk, struct rb_table* _fwd_sk_set)
 			break;
 		}
 	} while (likely((cur_fwd_sk = rb_t_next(&iter)) != NULL));
+	_tsk->fwd_sk_addr = _tsk->fwd_sk->fwd->addr;
 
 	return;
 }

types.h
@@ -87,7 +87,8 @@ struct ds_wrk_tsk
 	char fqdn[HOST_NAME_MAX];			// |
 	ldns_rr_type rr_type;				// | key
 	ldns_rr_class rr_class;				// |
-	struct ds_fwd_sk* fwd_sk;			// ]
+	struct ds_fwd_sk* fwd_sk;			// |
+	struct pfcq_net_addr fwd_sk_addr;	// ]
 
 	struct pfcq_net_addr addr;			// ]
 	uint16_t orig_id;					// |

commit 1da0c66b15b4a10bdd6b06cb650627615e2bd305

handlers: use pfcq_net_addr_cmp() again
For WDT reply handler I assumed we might want to compare
address only without port. This is not true, generally,
so revert back to former pfcq_net_addr_cmp() usage.

This commit renames pfcq_net_addr_port_cmp() back to
pfcq_net_addr_cmp() and removes pure address comparison
helper.

Fixes: 9a57e4a346817509493eed67bce3abeef23bd669
Signed-off-by: Oleksandr Natalenko <oleksandr@redhat.com>

handlers.c
@@ -226,7 +226,7 @@ int ds_wrk_rep_handler(int _fd, struct ds_wrk_ctx* _data)
 		rb_t_init(&iter, _data->fe_sk_set);
 		cur = rb_t_first(&iter, _data->fe_sk_set);
 		do {
-			if (pfcq_net_addr_port_cmp(&cur->fe->addr, &tsk->orig_fe_addr))
+			if (pfcq_net_addr_cmp(&cur->fe->addr, &tsk->orig_fe_addr))
 			{
 				sk = cur->sk;
 				found = true;

pfcq.c
@@ -431,26 +431,6 @@ bool pfcq_net_addr_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2)
 		switch (_na1->family)
 		{
 			case AF_INET:
-				return _na1->addr.ip4.sin_addr.s_addr == _na2->addr.ip4.sin_addr.s_addr;
-				break;
-			case AF_INET6:
-				return _na1->addr.ip6.sin6_addr.s6_addr == _na2->addr.ip6.sin6_addr.s6_addr;
-				break;
-			default:
-				panic("Unknown address family");
-				break;
-		}
-	} else
-		return false;
-}
-
-bool pfcq_net_addr_port_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2)
-{
-	if (_na1->family == _na2->family)
-	{
-		switch (_na1->family)
-		{
-			case AF_INET:
 				return ((_na1->addr.ip4.sin_addr.s_addr == _na2->addr.ip4.sin_addr.s_addr) &&
 						(_na1->addr.ip4.sin_port == _na2->addr.ip4.sin_port));
 				break;

pfcq.h
@@ -129,7 +129,6 @@ void pfcq_spin_done(pthread_spinlock_t* _lock);
 uint64_t pfcq_fast_hash(const uint8_t* _data, size_t _data_size, uint64_t _seed) __attribute__((warn_unused_result));
 
 bool pfcq_net_addr_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2) __attribute__((warn_unused_result));
-bool pfcq_net_addr_port_cmp(struct pfcq_net_addr* _na1, struct pfcq_net_addr* _na2) __attribute__((warn_unused_result));
 
 void pfcq_counter_init(struct pfcq_counter* _counter);
 void pfcq_counter_reset(struct pfcq_counter* _counter);