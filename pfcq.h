LanetNetwork/dnsbalancer

commit 592faaff7ca5d853799c6282dcb9d6a679d56abc

core: refactor pfcq to follow single-point exit approach

pfcq.h
@@ -44,7 +44,7 @@
 
 #define warning(A)				__pfcq_warning(A, errno, __FILE__, __LINE__, 1)
 #define fail(A)					__pfcq_fail(A, errno)
-#define stop(A)					__pfcq_stop(A)
+#define stop(A)					__pfcq_stop(EX_SOFTWARE, A)
 #define panic(A)				__pfcq_panic(A, errno, __FILE__, __LINE__)
 
 #define pfcq_zero(A, B)			pfcq_memset_g(A, 0, B)
@@ -88,8 +88,8 @@ struct pfcq_counter
 void __pfcq_debug(int _direct, const char* _format, ...) __attribute__((format(printf, 2, 3)));
 void __pfcq_warning(const char* _message, const int _errno, const char* _file, int _line, int _direct);
 void __pfcq_fail(const char* _message, const int _errno);
-void __pfcq_stop(const char* _message) __attribute__((noreturn));
-void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line) __attribute__((noreturn));
+void __pfcq_stop(const int _exit_code, const char* _message) __attribute__((noreturn));
+void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line);
 void pfcq_debug_init(int _verbose, int _debug, int _syslog);
 void pfcq_debug_done(void);
 

pfcq.c
@@ -121,16 +121,17 @@ void __pfcq_fail(const char* _message, const int _errno)
 	return;
 }
 
-void __pfcq_stop(const char* _message)
+void __pfcq_stop(const int _exit_code, const char* _message)
 {
-	__pfcq_debug(1, "%s\n", _message);
-	exit(EX_SOFTWARE);
+	if (likely(_message))
+		__pfcq_debug(1, "%s\n", _message);
+	exit(_exit_code);
 }
 
 void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line)
 {
 	__pfcq_warning(_message, _errno, _file, _line, 0);
-	exit(EX_SOFTWARE);
+	__pfcq_stop(EX_SOFTWARE, NULL);
 }
 
 void pfcq_debug_init(int _verbose, int _debug, int _syslog)

commit f5c67d7dff56a6328720396fd71bd8142cdfc49f

core: mark panic as noreturn again
Somehow compiler cannot realize that noreturn stop is called
from panic. Thus, suggest this to him.

This commit marks panic as noreturn again.

Fixes: 592faaff7ca5d853799c6282dcb9d6a679d56abc
Signed-off-by: Oleksandr Natalenko <oleksandr@redhat.com>

pfcq.h
@@ -90,7 +90,7 @@ void __pfcq_debug(int _direct, const char* _format, ...) __attribute__((format(p
 void __pfcq_warning(const char* _message, const int _errno, const char* _file, int _line, int _direct);
 void __pfcq_fail(const char* _message, const int _errno);
 void __pfcq_stop(const int _exit_code, const char* _message) __attribute__((noreturn));
-void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line);
+void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line) __attribute__((noreturn));
 void pfcq_debug_init(int _verbose, int _debug, int _syslog);
 void pfcq_debug_done(void);
 