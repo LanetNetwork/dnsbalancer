LanetNetwork/dnsbalancer

commit 592faaff7ca5d853799c6282dcb9d6a679d56abc

core: refactor pfcq to follow single-point exit approach

pfcq.c
@@ -121,16 +121,17 @@ void __pfcq_fail(const char* _message, const int _errno)
 	return;
 }
 
-void __pfcq_stop(const char* _message)
+void __pfcq_stop(const int _exit_code, const char* _message)
 {
-	__pfcq_debug(1, "%s\n", _message);
-	exit(EX_SOFTWARE);
+	if (likely(_message))
+		__pfcq_debug(1, "%s\n", _message);
+	exit(_exit_code);
 }
 
 void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line)
 {
 	__pfcq_warning(_message, _errno, _file, _line, 0);
-	exit(EX_SOFTWARE);
+	__pfcq_stop(EX_SOFTWARE, NULL);
 }
 
 void pfcq_debug_init(int _verbose, int _debug, int _syslog)

pfcq.h
@@ -44,7 +44,7 @@
 
 #define warning(A)				__pfcq_warning(A, errno, __FILE__, __LINE__, 1)
 #define fail(A)					__pfcq_fail(A, errno)
-#define stop(A)					__pfcq_stop(A)
+#define stop(A)					__pfcq_stop(EX_SOFTWARE, A)
 #define panic(A)				__pfcq_panic(A, errno, __FILE__, __LINE__)
 
 #define pfcq_zero(A, B)			pfcq_memset_g(A, 0, B)
@@ -88,8 +88,8 @@ struct pfcq_counter
 void __pfcq_debug(int _direct, const char* _format, ...) __attribute__((format(printf, 2, 3)));
 void __pfcq_warning(const char* _message, const int _errno, const char* _file, int _line, int _direct);
 void __pfcq_fail(const char* _message, const int _errno);
-void __pfcq_stop(const char* _message) __attribute__((noreturn));
-void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line) __attribute__((noreturn));
+void __pfcq_stop(const int _exit_code, const char* _message) __attribute__((noreturn));
+void __pfcq_panic(const char* _message, const int _errno, const char* _file, int _line);
 void pfcq_debug_init(int _verbose, int _debug, int _syslog);
 void pfcq_debug_done(void);
 

commit dc08bfc60d69fa210d91c965731b2032b4ad7f83

core: fix sysexits.h usage
pfcq should provide exit primitives. We have to get rid of multiple
sysexits.h inclusions.

This commits move sysexits.h inclusion to pfcq.h.

Fixes: 592faaff7ca5d853799c6282dcb9d6a679d56abc
Signed-off-by: Oleksandr Natalenko <oleksandr@redhat.com>

main.c
@@ -29,7 +29,6 @@
 
 #include <getopt.h>
 #include <signal.h>
-#include <sysexits.h>
 
 #include "types.h"
 

pfcq.c
@@ -30,7 +30,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sysexits.h>
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>

pfcq.h
@@ -27,6 +27,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/time.h>
+#include <sysexits.h>
 #include <time.h>
 
 #include "sys.h"