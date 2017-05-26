LanetNetwork/dnsbalancer

commit 790bd5b11a08c99b2caac3077e01eba186c4feb3

core: use signalfd instead of sigaction+sigsuspend
signalfd is easy to use, producing much more readable code.

This commit replaces sigaction+sigsuspend with signalfd+read.
This removes signals.{c,h} files, getting rid of externs
in main.c

Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

main.c
@@ -29,18 +29,15 @@
 
 #include <getopt.h>
 #include <signal.h>
+#include <sys/signalfd.h>
 #include <sysexits.h>
 
 #include "types.h"
 
 #include "context.h"
 #include "pfcq.h"
-#include "signals.h"
 #include "utils.h"
 #include "worker.h"
-
-extern sig_atomic_t should_exit;
-extern sig_atomic_t should_reload;
 
 int main(int _argc, char** _argv)
 {
@@ -49,9 +46,9 @@ int main(int _argc, char** _argv)
 	int be_verbose = 0;
 	int do_debug = 0;
 	int use_syslog = 0;
+	int sfd = -1;
 	char* pid_file = NULL;
 	char* config_file = NULL;
-	struct sigaction ds_sigaction;
 	sigset_t ds_newmask;
 	sigset_t ds_oldmask;
 	struct ds_ctx* ctx = NULL;
@@ -67,7 +64,6 @@ int main(int _argc, char** _argv)
 		{0, 0, 0, 0}
 	};
 
-	pfcq_zero(&ds_sigaction, sizeof(struct sigaction));
 	pfcq_zero(&ds_newmask, sizeof(sigset_t));
 	pfcq_zero(&ds_oldmask, sizeof(sigset_t));
 
@@ -119,49 +115,54 @@ int main(int _argc, char** _argv)
 			panic("fclose");
 	}
 
-	ds_sigaction.sa_handler = ds_sigall_handler;
-	sigemptyset(&ds_sigaction.sa_mask);
-	ds_sigaction.sa_flags = 0;
-	sigaction(SIGTERM, &ds_sigaction, NULL);
-	sigaction(SIGINT, &ds_sigaction, NULL);
-	sigaction(SIGUSR1, &ds_sigaction, NULL);
 	sigemptyset(&ds_newmask);
 	sigaddset(&ds_newmask, SIGTERM);
 	sigaddset(&ds_newmask, SIGINT);
 	sigaddset(&ds_newmask, SIGUSR1);
 	pthread_sigmask(SIG_BLOCK, &ds_newmask, &ds_oldmask);
 
+	sfd = signalfd(-1, &ds_newmask, 0);
+	if (unlikely(sfd == -1))
+		panic("signalfd");
+
 	// context
 	ctx = ds_ctx_load(config_file);
 
 	while (true)
 	{
-		sigsuspend(&ds_oldmask);
+		struct signalfd_siginfo si;
+		ssize_t res = -1;
 
-		if (should_exit)
+		pfcq_zero(&si, sizeof(struct signalfd_siginfo));
+
+		res = ds_read(sfd, &si, sizeof(struct signalfd_siginfo));
+		if (unlikely(res == -1 || res != sizeof(struct signalfd_siginfo)))
+			panic("ds_read");
+
+		switch (si.ssi_signo)
 		{
-			ds_ctx_unload(ctx);
+			case SIGINT:
+			case SIGTERM:
+				ds_ctx_unload(ctx);
+				goto out;
+			case SIGUSR1:
+				ctx_next = ds_ctx_load(config_file);
 
-			goto out;
-		}
+				ctx->ctx_next = ctx_next;
+				ctx->redirect = true;
 
-		if (should_reload)
-		{
-			ctx_next = ds_ctx_load(config_file);
+				ds_ctx_unload(ctx);
 
-			ctx->ctx_next = ctx_next;
-			ctx->redirect = true;
+				ctx = ctx_next;
 
-			ds_ctx_unload(ctx);
-
-			ctx = ctx_next;
-
-			should_reload = 0;
-			continue;
+				continue;
+			default:
+				panic("Unknown signal");
 		}
 	}
 
 out:
+	ds_close(sfd);
 	pthread_sigmask(SIG_UNBLOCK, &ds_newmask, NULL);
 
 	verbose("%s\n", "Ciao.");

CMakeLists.txt
@@ -15,7 +15,6 @@ add_executable(dnsbalancer
 	main.c
 	pfcq.c
 	rb.c
-	signals.c
 	utils.c
 	worker.c)
 

signals.c
@@ -1,49 +1,0 @@
-/* vim: set tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab */
-
-/*
- * dnsbalancer - daemon to balance UDP DNS requests over DNS servers
- * Initially created under patronage of Lanet Network
- * Programmed by Oleksandr Natalenko <oleksandr@natalenko.name>, 2015-2017
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include <signal.h>
-
-#include "pfcq.h"
-
-#include "signals.h"
-
-volatile sig_atomic_t should_exit = 0;
-volatile sig_atomic_t should_reload = 0;
-
-void ds_sigall_handler(int _signo)
-{
-	switch (_signo)
-	{
-		case SIGINT:
-		case SIGTERM:
-			if (likely(!should_exit))
-				should_exit = 1;
-			break;
-		case SIGUSR1:
-			if (likely(!should_reload))
-				should_reload = 1;
-			break;
-		default:
-			break;
-	}
-
-	return;
-}
-

signals.h
@@ -1,24 +1,0 @@
-/* vim: set tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab */
-
-/*
- * dnsbalancer - daemon to balance UDP DNS requests over DNS servers
- * Initially created under patronage of Lanet Network
- * Programmed by Oleksandr Natalenko <oleksandr@natalenko.name>, 2015-2017
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#pragma once
-
-void ds_sigall_handler(int _signo);
-

commit ab09f90a2fc1484604cb3a9ea55361ffdef7dafa

core: there is no newmask, just mask
Signal mask is the one and only.

This commit renames ds_newmask to ds_sigmask.

Fixes: 790bd5b11a08c99b2caac3077e01eba186c4feb3
Signed-off-by: Oleksandr Natalenko <oleksandr@redhat.com>

main.c
@@ -48,7 +48,7 @@ int main(int _argc, char** _argv)
 	int use_syslog = 0;
 	int sfd = -1;
 	char* config_file = NULL;
-	sigset_t ds_newmask;
+	sigset_t ds_sigmask;
 	struct ds_ctx* ctx = NULL;
 	struct ds_ctx* ctx_next = NULL;
 
@@ -61,7 +61,7 @@ int main(int _argc, char** _argv)
 		{0, 0, 0, 0}
 	};
 
-	pfcq_zero(&ds_newmask, sizeof(sigset_t));
+	pfcq_zero(&ds_sigmask, sizeof(sigset_t));
 
 	while ((opts = getopt_long(_argc, _argv, "abcde", longopts, NULL)) != -1)
 	{
@@ -97,13 +97,13 @@ int main(int _argc, char** _argv)
 		if (unlikely(daemon(0, 0) != 0))
 			panic("daemon");
 
-	sigemptyset(&ds_newmask);
-	sigaddset(&ds_newmask, SIGTERM);
-	sigaddset(&ds_newmask, SIGINT);
-	sigaddset(&ds_newmask, SIGUSR1);
-	pthread_sigmask(SIG_BLOCK, &ds_newmask, NULL);
+	sigemptyset(&ds_sigmask);
+	sigaddset(&ds_sigmask, SIGTERM);
+	sigaddset(&ds_sigmask, SIGINT);
+	sigaddset(&ds_sigmask, SIGUSR1);
+	pthread_sigmask(SIG_BLOCK, &ds_sigmask, NULL);
 
-	sfd = signalfd(-1, &ds_newmask, 0);
+	sfd = signalfd(-1, &ds_sigmask, 0);
 	if (unlikely(sfd == -1))
 		panic("signalfd");
 
@@ -145,7 +145,7 @@ int main(int _argc, char** _argv)
 
 out:
 	ds_close(sfd);
-	pthread_sigmask(SIG_UNBLOCK, &ds_newmask, NULL);
+	pthread_sigmask(SIG_UNBLOCK, &ds_sigmask, NULL);
 
 	verbose("%s\n", "Ciao.");
 