fba491184f720191e7281bdac7944f3ba74f8f62
Oleksandr Natalenko <oleksandr@natalenko.name>
2017-11-11 12:34:58 +0100

    build: undef _GNU_SOURCE since ICC is broken with it
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

--- cmake-scripts/generic.cmake
+++ cmake-scripts/generic.cmake
@@ -15,7 +15,7 @@ else ("${CMAKE_C_COMPILER_ID}" STREQUAL "Intel")
 endif ("${CMAKE_C_COMPILER_ID}" STREQUAL "Intel")
 
 set(CMAKE_C_FLAGS
-	"${DS_CONFIG_FLAGS} -std=c11 -D_DEFAULT_SOURCE -D_GNU_SOURCE -pipe -W -Wall -Wextra -pedantic -Wwrite-strings -Winit-self -Wcast-qual -Wpointer-arith -Wstrict-aliasing -Wformat=2 -Wmissing-declarations -Wmissing-include-dirs -Wno-unused-parameter -Wuninitialized -Wold-style-definition -Wstrict-prototypes -Wmissing-prototypes")
+	"${DS_CONFIG_FLAGS} -std=c11 -D_DEFAULT_SOURCE -pipe -W -Wall -Wextra -pedantic -Wwrite-strings -Winit-self -Wcast-qual -Wpointer-arith -Wstrict-aliasing -Wformat=2 -Wmissing-declarations -Wmissing-include-dirs -Wno-unused-parameter -Wuninitialized -Wold-style-definition -Wstrict-prototypes -Wmissing-prototypes")
 set(CMAKE_C_FLAGS_DEBUG "${DS_CONFIG_FLAGS} -O1 -g -ggdb -pg -mtune=generic -D_FORTIFY_SOURCE=2 -fstack-protector-all -DMODE_DEBUG")
 set(CMAKE_C_FLAGS_RELEASE "${DS_CONFIG_FLAGS} -O3 -march=native -mtune=native -D_FORTIFY_SOURCE=1 -fstack-protector-strong -DMODE_NORMAL")
 
